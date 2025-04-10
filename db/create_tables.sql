0f1c94ec59d9bb393b95b10ab8ca4b40b45cc8ad
JD-Profe <jdr958@educa.madrid.org>
2025-04-10 20:16:48 +0200

    Update create_tables.sql

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,1 +1,9 @@
+-- db/create_tables.sql
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    precio NUMERIC(10, 2) NOT NULL,
+    descripcion TEXT,
+    stock INTEGER DEFAULT 0
+);
 
